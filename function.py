781ba62eb6cb15cc72cc59f0587406e87439b9ae
baptistescancar <baptiste.scancar@agrocampus-ouest.fr>
2025-11-19 12:03:00 +0100

    v1.2

--- function.py
+++ function.py
@@ -1,8 +1,12 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
+from tqdm import tqdm
 import re
+from PubPeer_Scraper.driver import get_driver
 
 def get_pubpeer_comment_number(pmid, driver):
 
@@ -10,20 +14,62 @@ def get_pubpeer_comment_number(pmid, driver):
 
     driver.get(url)
 
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[href^='/publications']")))
+    try: 
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
+        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[href^='/publications']")))
 
-    footer = soup.find("div", class_="panel-footer")
-    span = footer.find("span", class_="pull-right") if footer else None
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
 
-    if not span:
-        count = 0
+        footer = soup.find("div", class_="panel-footer")
+        span = footer.find("span", class_="pull-right") if footer else None
+
+        if not span:
+            count = 0
+        else:
+            text = span.get_text(strip=True).lower()
+            m = re.search(r"(\d+)\s*comment", text)
+            count = int(m.group(1)) if m else 0
+
+        return count
+    
+    except TimeoutException:
+        print(f"No result for PMID: {pmid}")
+        return 0
+    
+    except Exception:
+        print(f"Error for PMID: {pmid} - code: {Exception}")
+        return 0
+
+
+def worker(pmid):
+    driver = get_driver()
+    result = get_pubpeer_comment_number(pmid, driver)
+    driver.quit()
+    return result
+
+def process_pmid_list(pmids, parallelise=False, num_workers=2):
+
+    if parallelise == True:  
+
+        print("Parallel processing")
+        with Pool(processes=num_workers) as p:
+            result_list = list(tqdm(
+                p.imap(worker, pmids),
+                total=len(pmids)
+            ))
+
+        return result_list
+    
     else:
-        text = span.get_text(strip=True).lower()
-        m = re.search(r"(\d+)\s*comment", text)
-        count = int(m.group(1)) if m else 0
+        print("Sequential processing")
+        driver = get_driver()
+        result_list = []
 
-    return count
+        for pmid in tqdm(pmids):
 
+            result = get_pubpeer_comment_number(pmid, driver)
+            print(f"Article PMID: {pmid} --- Number of comments: {result}")
+            result_list.append(result)
+            
+        return result_list
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from PubPeer_Scraper.function import get_pubpeer_comment_number
+from PubPeer_Scraper.function import process_pmid_list
 from PubPeer_Scraper.driver import get_driver
 
 driver=get_driver()
 
-pmid_list = ["37781291", "26808342", "25412939"] 
+import pandas as pd
 
-for pmid in pmid_list: 
-    get_pubpeer_comment_number(pmid, driver)
+# pmid_list = ["37781291", "26808342", "25412939"] 
+
+df = pd.read_excel("./integrity_experts.xlsx")
+
+df = df[-100:]
+
+result = process_pmid_list(df['PMID'], parallelise=True, num_workers=4)
+
+df['has_pub_peer_comment'] = result
+df[df['has_pub_peer_comment'] > 0]
 
 driver.quit()
+
