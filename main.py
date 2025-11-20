cbdcc80126d4292a49bf1b22b7136eb0b0d98457
Baptiste Scancar <baptiste.scancar@agrocampus-ouest.fr>
2025-11-20 09:54:00 +0100

    v1.4.1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ df = pd.read_excel("./PubPeer_Scraper/test_set.xlsx") #the fonctionnality of thi
 result = process_pmid_list(df['PMID'],
                            get_comment=True, #Extracts the content of the comments
                            ### IMPORTANT: please request access to https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct before turning sentiment analysis on. Then pass your HF token to your working environment.
-                           sentiment_analysis=True, #use LLM to analyse the nature of the comments (Commenting the study, Discussion the methods, Reporting incoherences...)
+                           sentiment_analysis=True, #use LLM to analyse the nature of the comments (Commenting the study, Discussing the methods, Reporting incoherences...)
                            ### IMPORTANT: parallelism will be automatically disabled when performing sentiment analysis.
                            parallelise=False, #speed up the process (true for total number of requests >50) #Parallelism is silent.
                            num_workers=4) #number of cores that will be used for multiparallelism (depends on your machine). Stay in the range 2-8 for PubPeer.
